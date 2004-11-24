2d7a76501ccc93662841bd42026ce7bb5d0c0419
sekhar <sekhar>
2004-11-24 16:57:02 +0000

    Issue number: Obtained from: Submitted by: Reviewed by: changed @TARGET from {FIELD, METHOD, TYPE} to {FIELD, METHOD} XmlRootElement (not XmlElement) must be used on a class.

--- trunk/src/javax/xml/bind/annotation/XmlElement.java
+++ trunk/src/javax/xml/bind/annotation/XmlElement.java
@@ -93,10 +93,10 @@ import static java.lang.annotation.RetentionPolicy.*;
  * 
  * @author Sekhar Vajjhala, Sun Microsystems, Inc.
  * @since JAXB2.0
- * @version $Revision: 1.6 $
+ * @version $Revision: 1.7 $
  */
 
-@Retention(RUNTIME) @Target({FIELD, METHOD, TYPE})
+@Retention(RUNTIME) @Target({FIELD, METHOD})
 public @interface XmlElement {
     /**
      *
