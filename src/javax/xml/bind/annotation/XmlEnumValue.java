d9bfe04d8e9a3415552f6546a4c95cd906966e8f
sekhar <sekhar>
2005-01-24 18:25:22 +0000

    Initial Version

---
+++ src/javax/xml/bind/annotation/XmlEnumValue.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2004 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package javax.xml.bind.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static java.lang.annotation.ElementType.FIELD;
+
+/**
+ * Maps an enum constant in {@link Enum} type to XML representation.  
+ * 
+ * <p> <b>Usage</b> </p>
+ *
+ * <p> The <tt>@XmlEnumValue</tt> annotation can be used with the
+ *     following program elements:  
+ * <ul> 
+ *   <li>enum constant</li>
+ * </ul>
+ *
+ * <p>See "Package Specification" in javax.xml.bind.package javadoc for
+ * additional common information.</p>
+ *
+ * <p>This annotation, together with {@link XmlEnum} provides a
+ * mapping of enum type to XML representation.
+ *
+ * <p>An enum type is mapped to a schema simple type with enumeration
+ * facets. The schema type is derived from the Java type specified in
+ * <tt>@XmlEnum.value()</tt>. Each enum constant <tt>@XmlEnumValue</tt>
+ * must have a valid lexical representation for the type
+ * <tt>@XmlEnum.value()</tt> 
+ *
+ * <p> In the absence of this annotation, {@link Enum#name()} is used
+ * as the XML representation.
+ *
+ * <p> <b>Example 1: </b>Map enum constant name -> enumeration facet</p>
+ * <pre>
+ *     //Example: Code fragment
+ *     &#64;XmlEnum(String.class)
+ *     public enum Card { CLUBS, DIAMONDS, HEARTS, SPADES }
+ *
+ *     &lt;!-- Example: XML Schema fragment -->
+ *     &lt;xs:simpleType name="Card">
+ *       &lt;xs:restriction base="xs:NCName"/>
+ *         &lt;xs:enumeration value="CLUBS"/>
+ *         &lt;xs:enumeration value="DIAMONDS"/>
+ *         &lt;xs:enumeration value="HEARTS"/>
+ *         &lt;xs:enumeration value="SPADES"/>
+ *     &lt;/xs:simpleType>
+ * </pre>
+ *
+ * <p><b>Example 2: </b>Map enum constant name(value) -> enumeration facet </p>
+ * <pre>
+ *     //Example: code fragment
+ *     &#64;XmlType
+ *     &#64;XmlEnum(Integer.class)
+ *     public enum Coin { PENNY(1), NICKEL(5), DIME(10), QUARTER(25) }
+ *     
+ *     &lt;!-- Example: XML Schema fragment -->
+ *     &lt;xs:simpleType name="Coin">
+ *       &lt;xs:restriction base="xs:int">
+ *         &lt;xs:enumeration value="1"/>
+ *         &lt;xs:enumeration value="5"/>
+ *         &lt;xs:enumeration value="10"/>
+ *         &lt;xs:enumeration value="25"/>
+ *       &lt;/xs:restriction>
+ *     &lt;/xs:simpleType>
+ * </pre>
+ *
+ * <p><b>Example 3: </b>Map enum constant name -> enumeration facet </p>
+ * 
+ *  <pre>
+ *     //Code fragment
+ *     &#64;XmlType
+ *     &#64;XmlEnum(Integer.class)
+ *     public enum Code {
+ *         &#64;XmlEnumValue("1") ONE,
+ *         &#64;XmlEnumValue("2") TWO;
+ *     }
+ * 
+ *     &lt;!-- Example: XML Schema fragment -->
+ *     &lt;xs:simpleType name="Code">
+ *       &lt;xs:restriction base="xs:int">
+ *         &lt;xs:enumeration value="1"/>
+ *         &lt;xs:enumeration value="2"/>
+ *       &lt;/xs:restriction>
+ *     &lt;/xs:simpleType>
+ * </pre>
+ *
+ */
+@Retention(RUNTIME)
+@Target({FIELD})
+public @interface XmlEnumValue {
+    String value();
+}
