8c62cc8678b0a6da4663279e0a31826c146c69ca
sekhar <sekhar>
2005-06-03 23:25:28 +0000

    Initial Version Issue number: Obtained from: Submitted by: Reviewed by:

---
+++ trunk/src/javax/xml/bind/annotation/AccessorOrder.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2005 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package javax.xml.bind.annotation;
+
+/**
+ * Used by XmlAccessorOrder to control the ordering of properties and
+ * fields in a JAXB bound class.
+ *
+ * @author Sekhar Vajjhala, Sun Microsystems, Inc.
+ * @since JAXB2.0
+ * @version $Revision: 1.1 $
+ * @see XmlAccessorOrder
+ */
+
+public enum AccessorOrder { 
+    /**
+     * The ordering of fields and properties in a class is undefined.
+     */
+    UNDEFINED,
+    /**
+     * The ordering of fields and properties in a class is in alphabetical order.
+     */
+    ALPHABETICAL
+}
+
