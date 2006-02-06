ba5d2f39857219032182bcadbb00ab254ec7302e
sekhar <sekhar>
2006-02-06 18:12:32 +0000

    Allow annotations on PARAMETER Issue number: Obtained from: Submitted by: Reviewed by:

--- trunk/src/javax/xml/bind/annotation/XmlAttachmentRef.java
+++ trunk/src/javax/xml/bind/annotation/XmlAttachmentRef.java
@@ -17,6 +17,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
 
 /**
  * Marks a field/property that its XML form is a uri reference to mime content.
@@ -56,6 +57,6 @@ import static java.lang.annotation.ElementType.METHOD;
  * @since JAXB2.0
  */
 @Retention(RUNTIME)
-@Target({FIELD,METHOD})
+@Target({FIELD,METHOD,PARAMETER})
 public @interface XmlAttachmentRef {
 }
--- trunk/src/javax/xml/bind/annotation/XmlMimeType.java
+++ trunk/src/javax/xml/bind/annotation/XmlMimeType.java
@@ -11,6 +11,7 @@ import java.awt.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
 
 import javax.xml.transform.Source;
 
@@ -30,7 +31,7 @@ import javax.xml.transform.Source;
  * @since JAXB2.0
  */
 @Retention(RUNTIME)
-@Target({FIELD,METHOD})
+@Target({FIELD,METHOD,PARAMETER})
 public @interface XmlMimeType {
     /**
      * The textual representation of the MIME type,
